c41104b11f4385b91819b128eb474f930f6a028e
ByteWiizard <ByteWiizard@gmail.com>
2024-06-12 13:57:33 +0530

    Convex and Clerk set up

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://intimate-goldfish-44.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
--- convex/http.ts
+++ convex/http.ts
@@ -1,2 +1,2 @@
 // webhook call for recieving data from external app such as clerk here
-
+    
